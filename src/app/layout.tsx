76406f9e707cc99f9aa837194acf1bf5fea6b549
samasama-dev <dev@samasama.app>
2026-04-03 10:52:55 +0000

    feat: Google Analytics（G-8S5GP8P7EZ）を全ページに追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -59,6 +60,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-8S5GP8P7EZ" />
       </body>
     </html>
   );
